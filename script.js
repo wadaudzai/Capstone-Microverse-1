67cd90c0ced6d7c2beb916f7ce50e25942ee3b42
wadaudzai <wadaudzai@gmail.com>
2022-07-01 01:02:38 +0430

    Add parts of the desktop version

--- script.js
+++ script.js
@@ -1,43 +1,55 @@
+// Object about the speakers section
 const speakers = [
     {
-        spimg: '',
-        spname: 'Narendra Modu',
+        spimg: './assets/images/narendra-modi.jpeg',
+        spname: 'Narendra Modi',
         spinfo: 'President of India',
-        spdetails: 'Narendra modi is the ',
+        spdetails: 'PM of India',
     },
     {
-        spimg: '',
+        spimg: './assets/images/ashraf-ghani.webp',
         spname: 'Ashraf Ghani',
         spinfo: 'President of Afghanistan',
-        spdetails: 'Ashraf Ghani is the second president of Republic of Afghanistan',
+        spdetails: 'President of Afghanistan',
     },
     {
-        spimg: '',
-        spname: '',
-        spinfo: '',
-        spdetails: '',
+        spimg: './assets/images/shahbaz-shareef.jpeg',
+        spname: 'Shahbaz Shareef',
+        spinfo: 'PM of Pakistan',
+        spdetails: 'PM of Pakistan',
     },
     {
-        spimg: '',
-        spname: '',
-        spinfo: '',
-        spdetails: '',
-    },
-    {
-        spimg: '',
-        spname: '',
-        spinfo: '',
-        spdetails: '',
-    },
-    {
-        spimg: '',
-        spname: '',
-        spinfo: '',
-        spdetails: '',
+        spimg: './assets/images/bangladesh-pm.jpeg',
+        spname: 'Sheikh Hasina',
+        spinfo: 'PM of Bangladesh',
+        spdetails: 'PM of Bangladesh',
     },
 ];
 
-// Hamburger menu
+// for each loop to create the section as per the members
+
+speakers.forEach((element, i) => {
+    const thirdSection = document.querySelector('.third-section');
+    const speakerCard = document.createElement('article');
+    speakerCard.classList = 'featured-speakers';
+  
+    const spCard = `
+    <div class="grid-two-parts">
+        <div class="featured-photo-holder">
+           <img src="${speakers[i].spimg}">
+        </div>
+        <div class="featured-text-holder">
+            <h3 class="featured-name">${speakers[i].spname}</h3>
+            <h4 class="featured-info">${speakers[i].spinfo}</h4>
+            <p class="featured-details">${speakers[i].spdetails}</p>
+        </div>
+    </div>
+  `;
+    speakerCard.innerHTML += spCard;
+    thirdSection.appendChild(speakerCard);
+  });
+
+  // Hamburger menu
 
 const hamburger = document.querySelector('.hamburger');
 const navMenu = document.querySelector('.nav-menu');
